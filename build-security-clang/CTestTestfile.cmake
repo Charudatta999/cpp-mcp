a795e98693486931163f37765418dbf37d14eb8d
Charudatta Jadhav <cjawsome999@gmail.com>
2026-05-19 10:26:41 +0530

    temp commit

--- build-security-clang/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: D:/cjadhav/Work/apps/cpp-mcp
-# Build directory: D:/cjadhav/Work/apps/cpp-mcp/build-security-clang
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[cpp_mcp_tests]=] "D:/cjadhav/Work/apps/cpp-mcp/build-security-clang/cpp_mcp_tests.exe")
-set_tests_properties([=[cpp_mcp_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "D:/cjadhav/Work/apps/cpp-mcp/CMakeLists.txt;134;add_test;D:/cjadhav/Work/apps/cpp-mcp/CMakeLists.txt;0;")
